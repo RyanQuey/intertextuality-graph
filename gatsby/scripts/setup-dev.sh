bb2f1996ba18b5c60d9b49d158dfec1bbe4de128
Ryan Quey <quey.ryan@gmail.com>
2020-09-15 18:22:17 +0000

    more scripts

---
+++ gatsby/scripts/setup-dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# script to run once when setting up a new development environment
+if [ "$BASH" != "/bin/bash" ]; then
+  echo "Please do ./$0"
+  exit 1
+fi
+
+# always base everything relative to this file to make it simple
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+. $parent_path/config/env-vars.sh
+
+cd $INTERTEXTUALITY_GRAPH_GATSBY_DIR && \
+  # run nvm use if they have nvm...
+  nvm use || true && \
+  npm i && \
+  gatsby develop

7d0e6d3aae224e261160a36f887092ec88c46c02
Ryan Quey <quey.ryan@gmail.com>
2020-09-23 00:04:25 +0000

    add more routes in play app

--- play-app/app/controllers/ConnectionsController.scala
+++ play-app/app/controllers/ConnectionsController.scala
@@ -25,9 +25,11 @@ import com.ryanquey.intertextualitygraph.modelhelpers.IntertextualConnection
 
 // way overkill, but just trying to find what works for method "out"
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal
 import org.apache.tinkerpop.gremlin.process.traversal.IO;
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.Operator._;
+import org.apache.tinkerpop.gremlin.process.traversal.Path;
 import org.apache.tinkerpop.gremlin.process.traversal.Order._;
 import org.apache.tinkerpop.gremlin.process.traversal.P._;
 import org.apache.tinkerpop.gremlin.process.traversal.Pop._;
@@ -57,6 +59,7 @@ class ConnectionsController @Inject()(cc: ControllerComponents) extends Abstract
    *
    * TODO 
    * - find a way to reuse the same implicit conversion throughout the different controllers
+   * - will need to try after we get everything set up...for now just using graphson instead
    *
    *   Example from https://www.playframework.com/documentation/2.8.x/ScalaJsonHttp#Serving-a-list-of-entities-in-JSON
   implicit val locationWrites: Writes[Location] =
@@ -64,7 +67,6 @@ class ConnectionsController @Inject()(cc: ControllerComponents) extends Abstract
 
   implicit val placeWrites: Writes[Place] =
     (JsPath \ "name").write[String].and((JsPath \ "location").write[Location])(unlift(Place.unapply))
-   */
 
   implicit val connectionWrites: Writes[IntertextualConnection] = (JsPath \ "sourceTextStartingBook").write[String].and(
     (JsPath \ "sourceTextId").write[UUID]).and(
@@ -81,12 +83,7 @@ class ConnectionsController @Inject()(cc: ControllerComponents) extends Abstract
     (JsPath \ "sourceVersion").write[Option[String]]).and(
     (JsPath \ "sourceLanguage").write[Option[String]])(unlift(IntertextualConnection.unapply))
 
-
-  val json_mapper = GraphSONMapper.
-                build().
-                version(GraphSONVersion.V1_0).
-                create().
-                createMapper()
+   */
 
   /**
    * Create an Action to render an HTML page.
@@ -101,9 +98,31 @@ class ConnectionsController @Inject()(cc: ControllerComponents) extends Abstract
     Ok(_findAllSourcesRecursivelyForRef("Genesis", 1, 1))
   }
 
+  // only get one
+  def findAllSourcesForRef() = Action { implicit request: Request[AnyContent] =>
+    Ok(_findAllSourcesForRef("Genesis", 1, 1))
+  }
+
   def findTextsByStartingRef() = Action { implicit request: Request[AnyContent] =>
     Ok(_findTextByStartingRef("Genesis", 1, 1))
   }
+
+  def findPathsForAllSourcesForRef() = Action { implicit request: Request[AnyContent] =>
+    val texts = _fetchTextByStartingRef("Genesis", 1, 1)
+      .toList()
+
+    val g : GraphTraversalSource = CassandraDb.graph
+    val connectionsWithFields = g.V(texts).                
+			out("intertextual_connection") // starting simple
+
+    val paths = _findPathsForTraversal(connectionsWithFields).toList
+    val output = json_mapper.writeValueAsString(paths)
+
+    Ok(output)
+  }
+
+  //////////////////////////////////////////////////
+  // graph traversals
 
   /**
    * http://www.doanduyhai.com/blog/?p=13301
@@ -114,67 +133,86 @@ class ConnectionsController @Inject()(cc: ControllerComponents) extends Abstract
    * https://docs.datastax.com/en/developer/java-driver/4.9/manual/core/dse/graph/
    */
   def _findAllSourcesRecursivelyForRef (book : String, chapter : Int, verse : Int)  = {
-    val g : GraphTraversalSource = CassandraDb.graph
-
-    // get the text
-    val texts = g.V().has("text", "starting_book", book)
-      .has("starting_chapter", chapter)
-      .has("starting_verse",  verse)
-      //.next() // just get first hit
-      // .valueMap(true) // can you also use this and get the properties even when you're going to traverse its edges later? Also can't use valueMap with next, it is one or the other
-      //.limit(1)
+    val texts = _fetchTextByStartingRef(book, chapter, verse)
       .toList()
 
+    val g : GraphTraversalSource = CassandraDb.graph
     val connectionsWithFields = g.V(texts)
 			.out("intertextual_connection")  // TODO can use repeat 4 times
 			.out("intertextual_connection") 
 			.out("intertextual_connection") 
 			.out("intertextual_connection") 
-			.valueMap()
-			.toList
 
-    val output = connectionsWithFields
-
-    val outputJson = json_mapper.writeValueAsString(output)
-
-    outputJson
+    _outputAllValuesForTraversal(connectionsWithFields)
   }
 
   // only goes one deep
   def _findAllSourcesForRef (book : String, chapter : Int, verse : Int)  = {
-    val g : GraphTraversalSource = CassandraDb.graph
-
-    // get the text
-    val texts = g.V().has("text", "starting_book", book)
-      .has("starting_chapter", chapter)
-      .has("starting_verse",  verse)
+    val texts = _fetchTextByStartingRef(book, chapter, verse)
       .toList()
 
+    val g : GraphTraversalSource = CassandraDb.graph
     val connectionsWithFields = g.V(texts).                
 			out("intertextual_connection") // starting simple
-			.valueMap()
-			.toList
 
-    val output = connectionsWithFields
-
-    val outputJson = json_mapper.writeValueAsString(output)
-
-    outputJson
+    _outputAllValuesForTraversal(connectionsWithFields)
   }
 
   def _findTextByStartingRef (book : String, chapter : Int, verse : Int)  = {
-    val g : GraphTraversalSource = CassandraDb.graph
+    val texts = _fetchTextByStartingRef(book, chapter, verse)
 
-    val texts = g.V().has("text", "starting_book", book)
+    _outputAllValuesForTraversal(texts)
+  }
+
+  //////////////////////////////////////////////////
+  // graph traversal builders
+
+  // NOTE returns traversal, doesn't actually hit the db yet until something is called on it
+  def _fetchTextByStartingRef (book : String, chapter : Int, verse : Int)  = {
+    val g : GraphTraversalSource = CassandraDb.graph
+    val texts : GraphTraversal[Vertex, Vertex] = g.V().has("text", "starting_book", book)
       .has("starting_chapter", chapter)
       .has("starting_verse",  verse)
+
+    texts
+  }
+
+
+  //////////////////////////////////////////////////
+  // output helpers
+  val json_mapper = GraphSONMapper.
+                build().
+                version(GraphSONVersion.V1_0).
+                create().
+                createMapper()
+
+
+  /*
+   * Note that you don't always want to use this, often you'll want to specify what values you want
+   * This returns all the values
+   *
+   *
+   * Note that `Any` is probably too broad. So far, Vertex or Path
+   */ 
+
+  def _outputAllValuesForTraversal (traversal : GraphTraversal[_, _]) : String = {
+    val hitsList = traversal
       .valueMap() // 
       .toList()
 
-    val outputJson = json_mapper.writeValueAsString(texts)
+    val outputJson = json_mapper.writeValueAsString(hitsList)
 
     outputJson
   }
 
-}
+  /*
+   * NOTE make sure the traversal has a "out" called on it
+   *
+   */
+  def _findPathsForTraversal (traversal : GraphTraversal[Vertex, Vertex]) = {
+    // our primary key for texts is id, so return only that
+    traversal.path().by("id")
+  }
 
+
+}

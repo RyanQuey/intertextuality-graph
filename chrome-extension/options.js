9d8f97de77c5bdd85d773e667949282ed6f0de05
Ryan Quey <quey.ryan@gmail.com>
2020-09-24 23:56:53 -0700

    now making http request from the actual web page...just the wrong one though

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -15,10 +15,7 @@ function constructOptions(kButtonColors) {
     button.addEventListener('click', function() {
       chrome.storage.sync.set({color: item}, function() {
         console.log('color is ' + item);
-		          fetch("http://localhost:9000/sources-for-ref-with-alluding-texts?book=Genesis&chapter=1")
-            .then(response => response.text())
-            .then(text => console.log(text))
-            .catch(error => console.error(error))
+
         return true;  // Will respond asynchronously.
       })
 	  
@@ -28,30 +25,6 @@ function constructOptions(kButtonColors) {
   
 
 }
-
-
-// Example POST method implementation:
-// From https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-
-
 
 
 

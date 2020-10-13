6dba032f1224a39bf4dcd5d7ab08f4fb9711fbea
Ryan Quey <quey.ryan@gmail.com>
2020-10-13 01:06:21 -0700

    try without procfile

--- play-app/scripts/run-prod-server.sh
+++ play-app/scripts/run-prod-server.sh
@@ -5,12 +5,5 @@ if [ "$BASH" != "/bin/bash" ]; then
 fi
 
 # heroku requires running on dynamic port based off of environment variable
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-arg1=${1:-""}
-
-# load all project environment variables
-. $parent_path/../../scripts/config/env-vars.sh && \
-
-cd $INTERTEXTUALITY_GRAPH_PLAY_API_DIR
 
 sbt "run $PORT"

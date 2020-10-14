47a07c460345597c45980503e7f1cb68e61502fb
Ryan Quey <quey.ryan@gmail.com>
2020-10-13 20:57:42 -0700

    working to connect to AWS production env

--- etl-tools/scripts/import-tsk-data.sh
+++ etl-tools/scripts/import-tsk-data.sh
@@ -9,9 +9,12 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # load all project environment variables
 . $parent_path/../../scripts/config/env-vars.sh
-printf "\n\n== Packaging using sbt ==\n" && \
 
-$parent_path/sbt/sbt.sh clean package
+if [ $1 != "--skip-package" ]; then
+  printf "\n\n== Packaging using sbt ==\n" && \
+  $parent_path/sbt/sbt.sh clean package
+fi
+
 cd $parent_path/..
 sbt "runMain com.ryanquey.intertextualitygraph.dataimporter.ImportTreasuryOfScriptureKnowledge"
 
